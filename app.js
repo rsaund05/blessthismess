b6f6efb82a1259efd0c9cc76e3239ea1bba09643
Rob Saunders <rsaund05@uoguelph.ca>
2021-02-17 08:58:02 -0500

    commiting so I can start another pull

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mongoose = require('mongoose');
+var favicon = require('serve-favicon');
 
 //db object setup
 mongoose.set('debug', true);
@@ -19,7 +20,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 
-//App init
+//App obj init
 var app = express();
 
 // view engine setup
@@ -27,6 +28,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 //Set up middleware before routing in request handling chain
+app.use(favicon(__dirname + '/public/images/favicon.ico'));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
