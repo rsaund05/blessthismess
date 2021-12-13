c4d815ac3ec965e3f9f6781e6afc7727cc77907c
Robert Saunders <rsaund05@icloud.com>
2021-12-13 14:33:22 -0500

    setting up structure

--- App.js
+++ App.js
@@ -4,18 +4,18 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+	<View style={styles.container}>
+		<Text>Sausage slaps</Text>
+		<StatusBar style="auto" />
+	</View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		
+		alignItems: 'center',
+		justifyContent: 'center',
+  	},
 });
