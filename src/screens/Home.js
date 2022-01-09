81e7ac583a5f5e2af2147ab7e4d2cfa288f270c2
Robert Saunders <rsaund05@icloud.com>
2022-01-09 16:36:42 -0500

    more data reorganizing, also implemented an encouragement string that changes based on percentage of tasks completed

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -7,20 +7,61 @@ import { AppleHeader } from "@freakycoder/react-native-header-view";
 import ProgressBar from 'react-native-progress/Bar';
 
 var current_date = moment().format("dddd, MMMM Do YYYY, h:mm a");
-var temp_1 = 1;
-var temp_2 = 5;
-var temp_3 = 0.4;
+var totalTasksDueToday = 5;
+var calendarEventsToday = 2;
+var tasksCompleted = 3;
+var tasksRemaining = totalTasksDueToday - tasksCompleted;
+var tasksPercentComplete = tasksCompleted / totalTasksDueToday;
 
-var summary_data_str_1 = `Items Due: ${temp_1}`;
-var summary_data_str_2 =  `Calendar Events: ${5}`;
+var summary_data_str_1 = `Tasks Remaining Today: ${tasksRemaining}`;
+var summary_data_str_2 =  `Calendar Events Today: ${calendarEventsToday}`;
 
-var DASH_ID_LIST = [{id: "12345678", name: "Kids", dash_items: ["Tasks", "Calendar","Reminders"]}, {id: "87654321", name: "Groceries & Coupons", dash_items: ["List", "Images", "Reminders"]}, {id: "11223344", name: "Mom's Work Schedule", dash_items: ["Calendar", "Reminders"]}, {id: "55667788", name: "Dad's Work Schedule", dash_items: ["Calendar", "Reminders"]}];
+var DASH_ID_LIST = [{id: "12345678", name: "Kids", dash_items: ["Task List", "Calendar","Reminder List"]}, {id: "87654321", name: "Groceries & Coupons", dash_items: ["List", "Reminder List", "Images"]}, {id: "11223344", name: "Mom's Work Schedule", dash_items: ["Calendar", "Reminder List"]}, {id: "55667788", name: "Dad's Work Schedule", dash_items: ["Calendar", "Reminder List"]}];
 
 
 const Home = ({ navigation }) => {
     const {colors} = useTheme();
     const [dateTimeUpdate, setDateTimeUpdate] = useState(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
-    
+    var encouragementStr = ""
+    if((tasksPercentComplete === 0) && totalTasksDueToday > 0) {
+        encouragementStr = "Better get started!";
+    } else if(((tasksPercentComplete > 0) && (tasksPercentComplete < 0.25)) && (totalTasksDueToday > 0)) {
+        encouragementStr = "Off to a great start!"
+    } else if((tasksPercentComplete >= 0.25) && (tasksPercentComplete < 0.5) && (totalTasksDueToday > 0)) {
+        encouragementStr = "Over a quarter done, nice pace!";
+    } else if((tasksPercentComplete === 0.5) && (totalTasksDueToday > 0)) {
+        encouragementStr = "Halfway there!";
+    } else if((tasksPercentComplete >= 0.5) && (tasksPercentComplete < 0.75) && (totalTasksDueToday > 0)) {
+        encouragementStr = "Over halfway there, keep it up!";
+    } else if((tasksPercentComplete >= 0.75) && (tasksPercentComplete < 1) && (totalTasksDueToday > 0)) {
+        encouragementStr = "So close to being done!";
+    } else if((tasksPercentComplete === 1) && (totalTasksDueToday > 0)) {
+        encouragementStr = "All tasks done for today, nice!";
+    }else {
+        encouragementStr="No tasks due today";
+    }
+    // switch(tasksPercentComplete) {
+    //     case (0) && (tasksDueToday > 0):
+    //         encouragementStr = "Better get started!";
+    //         break;
+    //     case ((tasksPercentComplete >= 0.25) && (tasksPercentComplete < 0.5)) && (tasksDueToday > 0):
+    //         encouragementStr = "Off to a great start!";
+    //         break;
+    //     case (tasksPercentComplete === 0.5) && (tasksDueToday > 0):
+    //         encouragementStr = "Halfway there!";
+    //         break;
+    //     case ((tasksPercentComplete >= 0.5) && (tasksPercentComplete < 0.75)) && (tasksDueToday > 0):
+    //         encouragementStr = "Over halfway there, keep it up!";
+    //         break;
+    //     case ((tasksPercentComplete >= 0.75) && (tasksPercentComplete < 1) && (tasksDueToday > 0)):
+    //         encouragementStr = "So close to being done!";
+    //         break;
+    //     case (tasksPercentComplete === 1) && (tasksDueToday > 0):
+    //         encouragementStr = "All tasks done for today, nice!";
+    //         break;
+    //     default:
+    //         encouragementStr = "No tasks due today";
+    // };
     const listHeaderText = {
         color: colors.text,
         fontWeight: 'bold',
@@ -65,11 +106,11 @@ const Home = ({ navigation }) => {
                 <View style={[cardStyle, {marginTop: 20}]}>
                     <Text style={summaryStyle}>{summary_data_str_1}</Text>
                     <Text style={summaryStyle}>{summary_data_str_2}</Text>
-                    
                 </View>
                 <View style={[cardStyle, {marginTop: 10, flexDirection: 'column'}]}>
-                    <Text style={[summaryStyle, {marginBottom: 5}]}>Daily Task Completion Progress: %{temp_3*100}</Text>
-                    <ProgressBar width={null}progress={temp_3}color={colors.primary}/>
+                    <Text style={[summaryStyle, {marginBottom: 5}]}>Today's Task Completion Progress: %{tasksPercentComplete*100}</Text>
+                    <ProgressBar width={null} progress={tasksPercentComplete}color={colors.primary}/>
+                    <Text style={[summaryStyle, {fontWeight: "normal", fontStyle: "italic"}]}>{encouragementStr}</Text>
                 </View>
                 {/* <SectionList
                     style={styles.list}
